5342723a077675b4af7a182a3ee2ed8e3110ca4b
Eloik-dev <eloik.rousseau@gmail.com>
2025-04-23 18:35:06 -0400

    Update

--- opencv_install.sh
+++ opencv_install.sh
@@ -3,6 +3,10 @@
 # Activate your virtual environment
 . .venv/bin/activate
 
+sudo apt-get update
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev -y
+
 # Set OpenCV build directory
 OPENCV_DIR="./opencv_build"
 OPENCV_VER="master"
@@ -25,13 +29,15 @@ export CMAKE_ARGS="
   -DWITH_GTK=ON
   -DWITH_OPENGL=ON
   -DWITH_V4L=ON
+  -DWITH_FFMPEG=OFF # Explicitly turn OFF FFmpeg
+  -DENABLE_PRECOMPILED_HEADERS=OFF # Sometimes helps with build issues
+  -DPKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig:/usr/share/pkgconfig
 "
 
 # Install numpy version compatible with Python 3.11
 pip install --upgrade numpy==1.26.4
 
-# Build the OpenCV wheel
-pip wheel . --verbose
+rm -rf opencv_build/_skbuild
 
 # Install the generated wheel
-pip install opencv_python*.whl
+pip install . --no-build-isolation -v
